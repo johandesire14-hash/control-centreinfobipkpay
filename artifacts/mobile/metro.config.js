ce01ad921197617482e568ca968f4c1217310199
johandesire14-hash <2.92765678e+08+johandesire14-hash@users.noreply.github.com>
2026-08-16 20:25:06 +0000

    fix(mobile): align Expo development build with working Replit project

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,22 +1,3 @@
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
-const config = getDefaultConfig(projectRoot);
-
-// Replit : ne pas faire surveiller tout le monorepo ni les caches générés.
-config.watchFolders = [
-  projectRoot,
-  path.join(workspaceRoot, "lib"),
-];
-
-config.resolver.blockList = [
-  /\/\.git\/.*$/,
-  /\/\.expo\/.*$/,
-  /\/node_modules\/.*\/node_modules\/.*$/,
-];
-
-config.maxWorkers = 2;
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
